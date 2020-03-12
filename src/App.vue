8abb205e993fddfe548f860639a95c6add7f7071
María Jesús Fuenzalida Serrano <maria.fuenzalida.serrano@gmail.com>
2020-03-12 17:44:10 -0300

    Primer avance con todo funcional (Falta agregar el responsivo)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,103 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah&display=swap" rel="stylesheet">
+    <!-- Titulo -->
+    <h1 class="title">Lista de Tareas</h1>
+    <!-- Formulario -->
+    <form class="container">
+      <label class="to-do">Tarea:</label>
+      <input type="text" v-model="tarea"/> 
+      <button class="form-b" @click.prevent="mostrarResult">Agregar Tarea</button>
+    </form>
+    <!-- Componente -->
+    <ListaTarea :tareas="tareas" @BorrarTarea="borrarTarea"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Componente
+import ListaTarea from './components/ListaTarea.vue'
 
+// Funciones
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ListaTarea
+  },
+  data() {
+    return {
+      tareas: [],
+      tarea: ''
+    }
+  },
+  methods: {
+    // Función para mostrar tarea y no agregar nada si hay campo vacío
+    mostrarResult: function() {
+      if(this.tarea != "")
+      this.tareas.push(this.tarea) 
+      this.tarea = ''
+    },
+    // Funcion quitar tarea
+    borrarTarea: function(index) {
+      this.tareas.splice(index, 1)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    body {
+      margin: 0;
+      background-image: url(./assets/fondo.jpg);
+      background-size: cover;
+      background-repeat: no-repeat;
+    }
+   /* Contenedor */
+    #app {
+        font-size: 1.3em;
+        font-family: 'Gloria Hallelujah', cursive;
+        text-align: center;
+        padding: 2em;
+    }
+    /* Titulo de la página */
+    .title {
+        font-size: 2.5em;
+        color: crimson;
+        text-decoration-line: underline;
+        margin-top: 0;
+    }
+    /* Sección container */
+    .to-do {
+        font-size: 1.5em;
+        color: crimson;
+        margin-bottom: 0;
+        margin-right: 1em;
+    }
+    input {
+        width: 30%;
+        background-color: white;
+        color: crimson;
+        text-align: center;
+        font-size: 1em;   
+        padding: 0.2em;   
+        border-radius: 1em;
+        border: 1px solid crimson;
+        margin-right: 1em;
+
+    }
+    .form-b {
+        width: 15%;
+        background-color: crimson;
+        color: white;
+        font-family: 'Gloria Hallelujah', cursive;
+        font-size: 0.8em;
+        font-weight: bold;
+        border-radius: 1em;
+        border: 1px solid crimson;
+        transition: .3s;
+    }
+    .form-b:hover {
+      background-color: white;
+      color: crimson;
+    }
 </style>
---
+++ src/components/ListaTarea.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="main">
+    <!-- Lista de tareas que se muestran en el index -->
+    <ul class="list-container" v-for="(tarea, index) in tareas" :key="index">
+       <li class="list-t">{{tarea}}<button class="list-b" @click="EliminarTarea" v-bind:value="index">Eliminar</button></li>
+     </ul>
+  </div>
+</template>
+
+<script>
+// Funciones
+export default {
+  name: 'ListaTarea',
+  props: {
+    tareas: Array
+  },
+  methods: {
+    // Función del boton para quitar la tarea
+    EliminarTarea: function(event) {
+      let index = event.target.value
+      this.$emit('BorrarTarea', index)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    /* Lista */
+    .list-container {
+        list-style: none;
+        display: flex;
+        width: 30%;
+        margin: auto;
+        margin-top: 1em;
+    }
+    .list-b {
+        width: 10em;
+        background-color: crimson;
+        color: white;
+        font-family: 'Gloria Hallelujah', cursive;
+        font-size: 0.7em;
+        font-weight: bold;
+        margin-left: 1em;
+        transition: .3s;
+        border: none;
+        border-radius: 1em;
+    }
+    .list-b:hover {
+        background-color: black;
+    }
+</style>
